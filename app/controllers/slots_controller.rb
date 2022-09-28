0845f8d619d310e45b3586493dbdf95673341e5d
faithadekunle <faith.adekunle.a@gmail.com>
2022-09-28 14:42:58 +0100

    get booked slots

--- app/controllers/slots_controller.rb
+++ app/controllers/slots_controller.rb
@@ -4,12 +4,14 @@ class SlotsController < ApplicationController
   def index; end
 
   def create
-    Slot.create!(start_time: params[:start_time], end_time: params[:end_time])
+    start_time = Date.parse(params[:date]).beginning_of_day
+    start_time.change(hour: params[:hour], min: params[:min])
+    Slot.create!(start_time: start_time, end_time: start_time + params[:duration].minutes)
 
     head :no_content, status: :created
   end
 
   def booked_slots
-    render json: BookedSlotsService.new.find_booked_slots(params[:date]), status: :ok
+    render(json: BookedSlotsService.new.find_booked_slots(params[:date], params[:interval_mins]), status: :ok)
   end
 end
--- app/services/booked_slots_service.rb
+++ app/services/booked_slots_service.rb
@@ -1,3 +1,15 @@
 class BookedSlotsService
-  def find_booked_slots(date); end
+  def find_booked_slots(date, interval_mins)
+    slots = Slot.where(start_time: date.all_day)
+
+    slots.each_with_object({}) do |slot, booked_slots|
+      end_time = slot.end_time
+      start_time = slot.start_time
+
+      while start_time < end_time
+        booked_slots["#{start_time.hour}:#{start_time.min}"] = true
+        start_time += interval_mins.minutes
+      end
+    end
+  end
 end
